747bdbd127f27627b8b5cd1d2ba09cf7c41b4baa
Arjan van de Ven <arjan@linux.intel.com>
2009-06-23 14:34:48 -0700

    start

---
+++ uxlaunch.c
@@ -0,0 +1,14 @@
+/*
+ * uxlaunch.c: Moblin starter
+ *
+ * (C) Copyright 2009 Intel Corporation
+ * Authors: 
+ *     Auke Kok <auke@linux.intel.com>
+ *     Arjan van de Ven <arjan@linux.intel.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ */
+
