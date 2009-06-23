2370e764705f53b398fb5fa01dafdd4e98e16ee6
Arjan van de Ven <arjan@linux.intel.com>
2009-06-23 14:48:53 -0700

    more template fill in

---
+++ desktop.c
@@ -0,0 +1,27 @@
+/*
+ * This file is part of uxlaunch
+ *
+ * (C) Copyright 2009 Intel Corporation
+ * Authors: 
+ *     Auke Kok <auke@linux.intel.com>
+ *     Arjan van de Ven <arjan@linux.intel.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "uxlaunch.h"
+
+void autostart_desktop_files(void)
+{
+}
+
+void start_metacity(void)
+{
+}
---
+++ xserver.c
@@ -0,0 +1,32 @@
+/*
+ * This file is part of uxlaunch
+ *
+ * (C) Copyright 2009 Intel Corporation
+ * Authors: 
+ *     Auke Kok <auke@linux.intel.com>
+ *     Arjan van de Ven <arjan@linux.intel.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "uxlaunch.h"
+
+void find_display_and_tty(void)
+{
+}
+
+void start_X_server(void)
+{
+}
+
+void wait_for_X_signal(void)
+{
+}
+ 
