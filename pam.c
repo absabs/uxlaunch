bb9691dccbefd959165fa716413b8270f6a2630b
Auke Kok <auke-jan.h.kok@intel.com>
2009-06-24 11:45:41 -0700

    Boilerplate PAM code.

--- pam.c
+++ pam.c
@@ -15,10 +15,49 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pwd.h>
+
+#include <security/pam_appl.h>
 
 #include "uxlaunch.h"
 
+pam_handle_t *ph;
+struct pam_conv pc;
+
 void setup_pam_session(void)
 {
+	char msg[256];
+	int err;
+
 	log_string("** Entering setup_pam_session");
+
+	err = pam_start("login", pass->pw_name, &pc, &ph);
+	err = pam_open_session(ph, 0);
+	if (err != PAM_SUCCESS) {
+		sprintf(msg, "pam_open_session returned %d: %s\n", err, pam_strerror(ph, err));
+		log_string(msg);
+		exit(1);
+	}
+
+	err = pam_set_item(ph, PAM_TTY, displaydev);
+	if (err != PAM_SUCCESS) {
+		sprintf(msg, "pam_set_item returned %d: %s\n", err, pam_strerror(ph, err));
+		log_string(msg);
+		exit(1);
+	}
+}
+
+void close_pam_session(void)
+{
+	char msg[256];
+	int err;
+
+	log_string("** Entering close_pam_session");
+
+	err = pam_close_session(ph, 0);
+	if (err) {
+		sprintf(msg, "pam_close_session returned %d: %s\n", err, pam_strerror(ph, err));
+		log_string(msg);
+	}
+	pam_end(ph, err);
 }
